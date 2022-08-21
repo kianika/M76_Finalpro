a03916da44633b0d1cb50102bae4b455980e29f8
kianika <mona_yaghoubi@gmail.com>
2022-08-21 16:32:09 +0200

    fetch update

--- finalproject/src/pages/orders/Orders.js
+++ finalproject/src/pages/orders/Orders.js
@@ -3,11 +3,9 @@ import Stack from "@mui/material/Stack";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import { useDispatch, useSelector } from "react-redux";
-import { loadorders, loadproducts } from "../../store/Orders";
+import { loadorders } from "../../store/orders";
+import { loadfilteredorders } from "../../store/filteredorders";
 import { useEffect } from "react";
-import { List } from "@mui/material";
-import { ListItem } from "@mui/material";
-import { ListItemText } from "@mui/material";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
@@ -17,7 +15,6 @@ import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 import Pagination from "@mui/material/Pagination";
 import { useState } from "react";
-import OrdersPagination from "./OrdersPagination";
 import Radio from "@mui/material/Radio";
 import RadioGroup from "@mui/material/RadioGroup";
 import FormControlLabel from "@mui/material/FormControlLabel";
@@ -26,39 +23,25 @@ import Link from "@mui/material/Link";
 
 const Orders = () => {
   const dispatch = useDispatch();
+  const filteredorders = useSelector((state) => state.filteredorders.list);
   const orders = useSelector((state) => state.orders.list);
 
-  const sentOrders = orders.filter((item) => item.delivered == "true");
-  const pendingOrders = orders.filter((item) => item.delivered == "false");
+  const [delivered, setDelivered] = useState(true);
+  let [page, setPage] = useState(1);
 
-  const [checked, setChecked] = useState("");
-
-  const radiohandleChange = (e) => {
-    const { name, value } = e.target;
-    setChecked(value);
-  };
+  console.log(orders);
+  
 
   useEffect(() => {
-    dispatch(loadorders());
-  }, [dispatch]);
+    dispatch(loadorders(delivered));
+  }, [delivered, dispatch]);
 
-  let [page, setPage] = useState(1);
-  const PER_PAGE = 5;
+  useEffect(() => {
+    dispatch(loadfilteredorders(delivered, page));
+  }, [delivered, page, dispatch]);
 
-  let _DATA = 0;
-
-  const count = Math.ceil(orders.length / PER_PAGE);
-  if (checked == "sent") {
-    _DATA = OrdersPagination(sentOrders, PER_PAGE);
-    const count = Math.ceil(orders.length / PER_PAGE);
-  } else {
-    _DATA = OrdersPagination(pendingOrders, PER_PAGE);
-  }
-
-  const handleChange = (e, p) => {
-    setPage(p);
-    _DATA.jump(p);
-  };
+  let total = Math.ceil(orders.length / 5);
+  console.log(total);
 
   return (
     <React.Fragment>
@@ -66,19 +49,25 @@ const Orders = () => {
         <FormControl component="fieldset">
           <RadioGroup
             aria-labelledby="demo-radio-buttons-group-label"
-            defaultValue="pending"
+            defaultValue="sent"
             name="radio-buttons-group"
-            onChange={radiohandleChange}
           >
             <FormControlLabel
               value="sent"
               control={<Radio />}
               label="سفارش های تحویل شده"
+              defaultChecked={true}
+              onClick={() => {
+                setDelivered(true);
+              }}
             />
             <FormControlLabel
               value="pending"
               control={<Radio />}
               label="سفارش های در انتظار ارسال"
+              onClick={() => {
+                setDelivered(false);
+              }}
             />
           </RadioGroup>
         </FormControl>
@@ -97,44 +86,38 @@ const Orders = () => {
             </TableRow>
           </TableHead>
           <TableBody>
-            {/*  {products.map((product) => (
-                    <ListItem key={product.id}>
-                        <ListItemText>{product.name}</ListItemText>
-                        </ListItem>
-                ))} */}
-
-            {_DATA.currentData().map((v) => (
-              <TableRow
-                key={v.id}
-                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-              >
-                <TableCell align="center" component="th" scope="row">
-                  <Link href="#">بررسی سفارش</Link>
-                </TableCell>
-                <TableCell align="center">
-                  {new Date(v.createdAt).getDay() +
-                    "/" +
-                    new Date(v.createdAt).getMonth() +
-                    "/" +
-                    new Date(v.createdAt).getFullYear()}
-                </TableCell>
-                <TableCell align="center">{v.prices}</TableCell>
-                <TableCell align="center">
-                  {v.username + " " + v.lastname}
-                </TableCell>
-              </TableRow>
-            ))}
+            {filteredorders.length &&
+              filteredorders.map((v) => (
+                <TableRow
+                  key={v.id}
+                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                >
+                  <TableCell align="center" component="th" scope="row">
+                    <Link href="#">بررسی سفارش</Link>
+                  </TableCell>
+                  <TableCell align="center">
+                    {new Date(v.createdAt).getDay() +
+                      "/" +
+                      new Date(v.createdAt).getMonth() +
+                      "/" +
+                      new Date(v.createdAt).getFullYear()}
+                  </TableCell>
+                  <TableCell align="center">{v.prices}</TableCell>
+                  <TableCell align="center">
+                    {v.username + " " + v.lastname}
+                  </TableCell>
+                </TableRow>
+              ))}
           </TableBody>
         </Table>
       </TableContainer>
 
       <Pagination
-        count={count}
+        count={total}
         size="large"
-        page={page}
         variant="outlined"
         shape="rounded"
-        onChange={handleChange}
+        onClick={(e) => setPage(e.target.textContent)}
       />
     </React.Fragment>
   );
--- finalproject/src/store/Store.js
+++ finalproject/src/store/Store.js
@@ -1,15 +1,17 @@
 import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import { productsReducer } from "./Products";
-import { ordersReducer } from "./Orders";
+import { ordersReducer } from "./orders";
 import Api from "./middleware/Api";
+import { filteredordersReducer } from "./filteredorders";
 export * from './Products';
-export * from './Orders';
+export * from './orders';
 
 export default function store() {
     return configureStore({
         reducer: {
             orders: ordersReducer,
-            products: productsReducer
+            products: productsReducer,
+            filteredorders: filteredordersReducer
         },
         middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(Api),
         })
